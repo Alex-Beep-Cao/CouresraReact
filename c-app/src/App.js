e99502f9d10132d8f5d24145f46f2683797dc3ec
Alex cao <hialexgoodmorning@gmail.com>
2021-11-27 12:20:39 +1300

    compont part2

--- c-app/src/App.js
+++ c-app/src/App.js
@@ -1,16 +1,40 @@
 import "./App.css";
 import { Navbar, NavbarBrand } from "reactstrap";
-
-function App() {
-  return (
-    <div className="App">
-      <Navbar dark color="primary">
-        <div className="container">
-          <NavbarBrand href="/"> Hello </NavbarBrand>
-        </div>
-      </Navbar>
-    </div>
-  );
+import Menu from "./components/MenuCompont";
+import { DISHES } from "./shared/dishes";
+import { Component } from "react";
+// import { useState } from "react";
+// function App() {
+// const [dishes, setdishes] = useState(DISHES);
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES,
+    };
+  }
+  render() {
+    return (
+      <div>
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/"> Hello </NavbarBrand>
+          </div>
+        </Navbar>
+        <Menu dishes={this.state.dishes}></Menu>
+      </div>
+    );
+  }
+  // return (
+  //   <div className="App">
+  //     <Navbar dark color="primary">
+  //       <div className="container">
+  //         <NavbarBrand href="/"> Hello </NavbarBrand>
+  //       </div>
+  //     </Navbar>
+  //     <Menu dishes={this.state.dishes} />
+  //   </div>
+  // );
 }
 
 export default App;
