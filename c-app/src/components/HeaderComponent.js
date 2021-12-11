7460cd4c605a9a8abb8b53c5b3d07a67356861a1
Alex cao <hialexgoodmorning@gmail.com>
2021-12-12 11:55:29 +1300

    router done

--- c-app/src/components/HeaderComponent.js
+++ c-app/src/components/HeaderComponent.js
@@ -1,12 +1,57 @@
-import React, { Component } from "react";
-import { Navbar, NavbarBrand, Jumbotron } from "reactstrap";
-
+import React, { useState, Component } from "react";
+import {
+  Navbar,
+  NavbarBrand,
+  Nav,
+  NavbarToggler,
+  Collapse,
+  NavItem,
+  Jumbotron,
+} from "reactstrap";
+import { NavLink } from "react-router-dom";
+// this.toggleNav = this.toggleNav.bind(this) old way
 const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleNav = () => {
+    setIsOpen(!isOpen);
+  };
   return (
-    <>
-      <Navbar dark>
+    <div>
+      <Navbar dark expand="md">
         <div className="container">
-          <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+          <NavbarToggler onClick={toggleNav} />
+          <NavbarBrand className="mr-auto" href="/">
+            <img
+              src="assets/images/logo.png"
+              height="30"
+              width="41"
+              alt="Ristorante Con Fusion"
+            />
+          </NavbarBrand>
+          <Collapse isOpen={isOpen} navbar>
+            <Nav navbar>
+              <NavItem>
+                <NavLink className="nav-link" to="/home">
+                  <span className="fa fa-home fa-lg"></span>Home
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink className="nav-link" to="/aboutus">
+                  <span className="fa fa-info fa-lg"></span> About Us
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink className="nav-link" to="/menu">
+                  <span className="fa fa-list fa-lg"></span> Menu
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink className="nav-link" to="/contactus">
+                  <span className="fa fa-address-card fa-lg"></span> Contact Us
+                </NavLink>
+              </NavItem>
+            </Nav>
+          </Collapse>
         </div>
       </Navbar>
       <Jumbotron>
@@ -23,16 +68,65 @@ const Header = () => {
           </div>
         </div>
       </Jumbotron>
-    </>
+    </div>
   );
 };
+
 // class Header extends Component {
+//   constructor(props) {
+//     super(props);
+
+//     this.toggleNav = this.toggleNav.bind(this);
+//     this.state = {
+//       isNavOpen: false,
+//     };
+//   }
+
+//   toggleNav() {
+//     this.setState({
+//       isNavOpen: !this.state.isNavOpen,
+//     });
+//   }
+
 //   render() {
 //     return (
-//       <>
-//         <Navbar dark>
+//       <div>
+//         <Navbar dark expand="md">
 //           <div className="container">
-//             <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+//             <NavbarToggler onClick={this.toggleNav} />
+//             <NavbarBrand className="mr-auto" href="/">
+//               <img
+//                 src="assets/images/logo.png"
+//                 height="30"
+//                 width="41"
+//                 alt="Ristorante Con Fusion"
+//               />
+//             </NavbarBrand>
+//             <Collapse isOpen={this.state.isNavOpen} navbar>
+//               <Nav navbar>
+//                 <NavItem>
+//                   <NavLink className="nav-link" to="/home">
+//                     <span className="fa fa-home fa-lg"></span> Home
+//                   </NavLink>
+//                 </NavItem>
+//                 <NavItem>
+//                   <NavLink className="nav-link" to="/aboutus">
+//                     <span className="fa fa-info fa-lg"></span> About Us
+//                   </NavLink>
+//                 </NavItem>
+//                 <NavItem>
+//                   <NavLink className="nav-link" to="/menu">
+//                     <span className="fa fa-list fa-lg"></span> Menu
+//                   </NavLink>
+//                 </NavItem>
+//                 <NavItem>
+//                   <NavLink className="nav-link" to="/contactus">
+//                     <span className="fa fa-address-card fa-lg"></span> Contact
+//                     Us
+//                   </NavLink>
+//                 </NavItem>
+//               </Nav>
+//             </Collapse>
 //           </div>
 //         </Navbar>
 //         <Jumbotron>
@@ -49,7 +143,7 @@ const Header = () => {
 //             </div>
 //           </div>
 //         </Jumbotron>
-//       </>
+//       </div>
 //     );
 //   }
 // }
--- c-app/src/components/MainComponent.js
+++ c-app/src/components/MainComponent.js
@@ -4,6 +4,8 @@ import Dishdetail from "./DishdetailCompont";
 import { Component } from "react";
 import Header from "./HeaderComponent";
 import Footer from "./FooterCompont";
+import Home from "./HomeComponent";
+import { Switch, Route, Redirect } from "react-router-dom";
 
 class Main extends Component {
   constructor(props) {
@@ -11,28 +13,31 @@ class Main extends Component {
 
     this.state = {
       dishes: DISHES,
-      selectedDish: null,
     };
-  }
-  onDishSelect(dishId) {
-    this.setState({ selectedDish: dishId });
   }
 
   render() {
+    const HomePage = () => {
+      return <Home />;
+    };
+
     return (
       <div>
         <Header />
-        <Menu
-          dishes={this.state.dishes}
-          onClick={(dishId) => this.onDishSelect(dishId)}
-        ></Menu>
-        <Dishdetail
-          dish={
-            this.state.dishes.filter(
-              (dish) => dish.id === this.state.selectedDish
-            )[0]
-          }
-        />
+        {/* can switch below views  */}
+        <Switch>
+          {/* component just view no props */}
+          <Route path="/home" component={HomePage} />
+          {/* if you want to pass the props, you should do a fucntional component  */}
+          <Route
+            exact
+            path="/menu"
+            component={() => <Menu dishes={this.state.dishes} />}
+          />
+          {/* the default route. if the nothing match return this default route  */}
+          <Redirect to="/home" />
+        </Switch>
+
         <Footer />
       </div>
     );
