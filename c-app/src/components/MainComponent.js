bc90d7b3554f974c16a57a1e4195000d296fffcc
Alex cao <hialexgoodmorning@gmail.com>
2021-12-12 21:11:11 +1300

    single page react part 1

--- c-app/src/components/MainComponent.js
+++ c-app/src/components/MainComponent.js
@@ -1,10 +1,13 @@
 import Menu from "./MenuCompont";
 import { DISHES } from "../shared/dishes";
-import Dishdetail from "./DishdetailCompont";
+import { COMMENTS } from "../shared/comments";
+import { PROMOTIONS } from "../shared/promotions";
+import { LEADERS } from "../shared/leaders";
 import { Component } from "react";
 import Header from "./HeaderComponent";
 import Footer from "./FooterCompont";
 import Home from "./HomeComponent";
+import Contact from "./ContactComponent";
 import { Switch, Route, Redirect } from "react-router-dom";
 
 class Main extends Component {
@@ -13,12 +16,24 @@ class Main extends Component {
 
     this.state = {
       dishes: DISHES,
+      comments: COMMENTS,
+      promotions: PROMOTIONS,
+      leaders: LEADERS,
     };
   }
 
   render() {
     const HomePage = () => {
-      return <Home />;
+      return (
+        <Home
+          dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+          promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+          leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+        />
+      );
+    };
+    const ContactPage = () => {
+      return <Contact />;
     };
 
     return (
@@ -35,6 +50,8 @@ class Main extends Component {
             component={() => <Menu dishes={this.state.dishes} />}
           />
           {/* the default route. if the nothing match return this default route  */}
+
+          <Route exact path="/contactus" component={ContactPage} />
           <Redirect to="/home" />
         </Switch>
 
