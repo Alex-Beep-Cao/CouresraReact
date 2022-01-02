9ebc029c48920cfa00db6b67e68d80987c31d0c7
Alex cao <hialexgoodmorning@gmail.com>
2022-01-02 23:43:01 +1300

    fixed spelling

--- c-app/src/redux/configureStroe.js
+++ c-app/src/redux/configureStroe.js
@@ -1,7 +1,7 @@
 import { createStore, combineReducers } from "redux";
 import { Dishes } from "./dishes";
 import { Comments } from "./comments";
-import { Promotions } from "./promotions";
+import { Promitions } from "./promotions";
 import { Leaders } from "./leaders";
 
 export const ConfigureStore = () => {
@@ -9,7 +9,7 @@ export const ConfigureStore = () => {
     combineReducers({
       dishes: Dishes,
       comments: Comments,
-      promotions: Promotions,
+      promotions: Promitions,
       leaders: Leaders,
     })
   );
